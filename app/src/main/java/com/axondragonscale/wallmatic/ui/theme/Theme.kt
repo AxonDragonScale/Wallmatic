62ce30f36fbe4b829c1ac74e2ec3c903acc8e3bc
AxonDragonScale <r.r.n.harkhani@gmail.com>
2024-06-05 23:16:16 +0530

    Update Dependencies

--- app/src/main/java/com/axondragonscale/wallmatic/ui/theme/Theme.kt
+++ app/src/main/java/com/axondragonscale/wallmatic/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.axondragonscale.wallmatic.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
