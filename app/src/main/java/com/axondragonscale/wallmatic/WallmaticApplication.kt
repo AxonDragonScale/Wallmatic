305d0101874963cf47dff20562457de14a16a7ae
AxonDragonScale <r.r.n.harkhani@gmail.com>
2024-06-05 23:23:42 +0530

    Add Application class for Hilt

---
+++ app/src/main/java/com/axondragonscale/wallmatic/WallmaticApplication.kt
@@ -0,0 +1,10 @@
+package com.axondragonscale.wallmatic
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Ronak Harkhani on 05/06/24
+ */
+@HiltAndroidApp
+class WallmaticApplication: Application()
