f8c922a73da150589ea39faa992782a4eb3a7532
AxonDragonScale <r.r.n.harkhani@gmail.com>
2024-10-09 23:45:45 +0530

    Corrected compose plugin naming convention

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ import com.google.protobuf.gradle.id
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.navigation.safeargs)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.protobuf)
